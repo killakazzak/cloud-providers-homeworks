ed59d4b3a9f5e3a7fcfabfecd5d28768ac81906e
Denis Ten <denis.a.ten@gmail.com>
2025-04-27 15:08:23 +0000

    modified:   storage.tf

--- storage.tf
+++ storage.tf
@@ -1,18 +1,18 @@
 # Создание Service Account для Object Storage
-resource "yandex_iam_service_account" "sa-tenda" {
-  name = "tenda-storage"
+resource "yandex_iam_service_account" "sa-tenda-storage" {
+  name = "sa-tenda-storage"
 }
 
 # Назначение роли storage.editor
 resource "yandex_resourcemanager_folder_iam_member" "bucket-editor" {
   folder_id = var.yc_folder_id
   role      = "storage.editor"
-  member    = "serviceAccount:${yandex_iam_service_account.sa-tenda.id}"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-tenda-storage.id}"
 }
 
 # Создание статических ключей доступа
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = yandex_iam_service_account.sa-tenda.id
+  service_account_id = yandex_iam_service_account.sa-tenda-storage.id
 }
 
 # Создание бакета
